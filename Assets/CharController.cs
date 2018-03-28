1f80c2a70bd23f72538cef43f08d7c38cb4d8cf8
an342 <an342@njit.edu>
2018-03-28 08:19:27 -0400

    added dash and heal

--- Assets/CharController.cs
+++ Assets/CharController.cs
@@ -1,13 +1,29 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CharController : MonoBehaviour {
 
-	// Use this for initialization
-    [SerializeField]
-    float movespeed = 4f;
+	// Gernal variables 
     Vector3 forward, right;
+    public float dashStrength;
+    public Rigidbody rb;
+    public DashState dashState;
+    public float dashTime;
+    public float dashCD;
+    public float iterate = 1f;
+    float timer;
+    public float HPtimer;
+
+    // Player Stats
+    public  float   HP;
+    public  Text    HPText;
+    public  float   MaxHP;
+    public  float   moveSpeed = 4f;
+    public  float   engery;
+    public  string  equipedWeapon;
+
 
 	void Start () 
     {
@@ -15,15 +31,38 @@ public class CharController : MonoBehaviour {
         forward.y = 0;
         forward = Vector3.Normalize(forward);
         right = Quaternion.Euler(new Vector3(0, 90, 0)) * forward;
+
+        StartCoroutine(UpdateHealth());
 	}
 	
 	// Update is called once per frame
 	void Update () 
     {
-        Move();    
-        Look();
-        //Look2();
-            
+        if (dashState == DashState.Ready || dashState == DashState.Cooldown)
+        {
+            Move();
+            Look();
+        }
+
+        if (Input.GetButtonDown("Square"))
+        {
+            if(dashState == DashState.Ready)
+            {
+                timer = 0;
+                StartCoroutine(DashCR());
+            }
+            else
+            {
+                Debug.Log("Dash on cooldown");
+            }
+        }
+        
+        if (Input.GetButtonDown("Triangle"))
+        {
+            Debug.Log("Hit Triangle");
+            StartCoroutine(HealthKit());
+        }
+        
 	}
 
     //point player front direction with right analog stick
@@ -38,30 +77,107 @@ public class CharController : MonoBehaviour {
         if(direction != new Vector3 (0,0,0))
             transform.forward = heading;
     }
-    void Look2()
-    {
-        Vector3 direction = new Vector3(Input.GetAxisRaw("RSH"), 0, Input.GetAxisRaw("RSV"));
-        Vector3 rightMovement = right * Input.GetAxisRaw("RSH");
-        Vector3 upMovement = forward * Input.GetAxisRaw("RSV");
-        Debug.Log("RSH: " + Input.GetAxis("RSH") + " " + Input.GetAxisRaw("RSH"));
-        Debug.Log("RSV: " + Input.GetAxis("RSV") + " " + Input.GetAxisRaw("RSV"));
-        Vector3 heading = Vector3.Normalize(rightMovement + upMovement);
-        if (direction != new Vector3(0, 0, 0))
-            transform.forward = heading;
-    }
 
     //move player with left analog stick
     void Move()
     {
         Vector3 direction = new Vector3(Input.GetAxis("LSH"), 0, Input.GetAxis("LSV"));
-        Vector3 rightMovement = right * movespeed * Time.deltaTime * Input.GetAxis("LSH");
-        Vector3 upMovement = forward * movespeed * Time.deltaTime * Input.GetAxis("LSV");
+        Vector3 rightMovement = right * moveSpeed * Time.deltaTime * Input.GetAxis("LSH");
+        Vector3 upMovement = forward * moveSpeed * Time.deltaTime * Input.GetAxis("LSV");
 
         //Vector3 heading = Vector3.Normalize(rightMovement + upMovement);
 
         //transform.forward = heading;
         transform.position += rightMovement;
         transform.position += upMovement;
+    }
+
+    IEnumerator UpdateHealth()
+    {
+        while (HP > 0)
+        {
+            HPText.text = "HP: " + HP + "/" + MaxHP;
+            yield return new WaitForSeconds(.1f);
+        }
+    }
+    void UseItem()
+    {
+
+    }
+    //Dash player, locks movement
+    void Dash()
+    {
+        Debug.Log("dash");
+        dashState = DashState.Dashing;
+        rb.AddForce(transform.forward * dashStrength);
+    }
+
+    //undos dash speed boost
+    void Hsad()
+    {
+        Debug.Log("Hsad");
+        rb.AddForce(-transform.forward * dashStrength);
+    }
+
+    IEnumerator HealthKit()
+    {
+        Debug.Log("Health Kit");
+        float timer = 0;
+        while(timer < HPtimer)
+        {
+            Debug.Log("Healing");
+            timer += .2f;
+            HP += 10;
+            yield return new WaitForSeconds(.2f);
+        }
+    }
+
+
+
+
+    IEnumerator DashCR()
+    {
+        Debug.Log("in DashCR");
+        Dash();
+
+        while (dashState == DashState.Dashing)
+        {
+            Debug.Log("Timer: " + timer + "  Dashtime" + dashTime);
+            if (timer > dashTime)
+            {
+                dashState = DashState.Cooldown;
+                Debug.Log("Dashstate now CD");
+                break;
+            }
+            Debug.Log("Dash while");
+            timer += 0.1f;
+            Debug.Log("Dash while2");
+            yield return new WaitForSeconds(0.1f);
+        }
+
+        Debug.Log("before Force removed");
+        Hsad();
+        Debug.Log("Force removed");
+
+        timer = 0;
+
+        while (timer < dashCD)
+        {
+            Debug.Log("CD while");
+            timer += 1;
+            yield return new WaitForSeconds(1);
+        }
+
+        Debug.Log("Dashstate ready");
+
+        dashState = DashState.Ready;
+    }
+
+    public enum DashState
+    {
+        Ready,
+        Dashing,
+        Cooldown
     }
 }
 
--- Assets/FireRaycast.cs
+++ Assets/FireRaycast.cs
@@ -2,50 +2,106 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FireRaycast : MonoBehaviour {
+public class FireRaycast : MonoBehaviour 
+{
 
 	// Use this for initialization
-    [SerializeField]
-    GameObject Player;
-    [SerializeField]
-    LayerMask mask;
-    [SerializeField]
-    GameObject hitEffect;
-    [SerializeField]
-    float RoF;
+    [SerializeField] GameObject Player;
+    [SerializeField] LayerMask mask;
+    [SerializeField] GameObject hitEffect;
+    [SerializeField] float RoF;
+    [SerializeField] string[] Weapons;
+    [SerializeField] string equipped;
     private float timer;
+    private bool semihold;
 
-
-	void Start () {
+	void Start () 
+    {
         mask = LayerMask.GetMask("Wall");
         timer = 0;
 	}
 	
 	// Update is called once per frame
-	void Update () {
-
-		
-	}
-
-    void FixedUpdate()
+	void Update () 
     {
 
-        RaycastHit hit;
-        Vector3 fwd = Player.transform.forward;
-        //Debug.Log(mask + " " + mask.value);
-        timer = timer + Time.deltaTime;
-       // Debug.Log(timer);
-        //Debug.Log("Time: " + Time.time + " Delta Time: " + Time.deltaTime + " Sub: " + (Time.time - Time.deltaTime));
-        if (timer >= RoF)
+	}
+     
+    void FixedUpdate()
+    {
+        timer += Time.deltaTime;
+        if (Input.GetButton("R2"))
         {
-            if (Physics.Raycast(transform.position, -fwd, out hit, 10, mask.value))
+            switch (equipped)
             {
-                timer = 0;
-                Debug.Log("i hit: " + hit.transform.name + " : " + hit.point);
-                Instantiate(hitEffect, hit.point, Quaternion.identity);
-
-
+                case "Pistol":
+                    FirePistol();
+                    break;
+                case "MG":
+                    FireMG();
+                    break;
+                case "Chain":
+                    FireChaingun();
+                    break;
+                default:
+                    Debug.Log("No weapon equipped!");
+                    break;
             }
         }
+        if (Input.GetButtonUp("R2"))
+        {
+            Debug.Log("button up");
+            semihold = false;
+        }
     }
-}
+
+    void FireMG()
+    {
+        Debug.Log("fire MG");
+        RoF = 0.2f;
+        if (timer > RoF)
+        {
+            FireBullet();
+            timer = 0;
+        }
+
+   }
+    void FirePistol()
+    {
+        Debug.Log("fire pistol");
+        Debug.Log("semihold: " + semihold);
+        if (timer > RoF && !semihold)
+        {
+            FireBullet();
+            timer = 0;
+            semihold = true;
+        }
+    }
+    void FireBullet()
+    {
+        Debug.Log("fire bullet");
+        RaycastHit hit;
+        Vector3 fwd = Player.transform.forward;
+        if (Physics.Raycast(transform.position, -fwd, out hit, 25, mask.value) )
+        {
+            Debug.Log("i hit: " + hit.transform.name + " : " + hit.point);
+            Instantiate(hitEffect, hit.point, Quaternion.identity);
+        }
+    }
+    void FireChaingun()
+    {
+        Debug.Log("fire chain");
+        RoF = 0.02f;
+        if (timer > RoF)
+        {
+            FireBullet();
+            timer = 0;
+        }
+    }
+    void ChangeWeapon()
+    {
+
+    }
+ }
+
+
